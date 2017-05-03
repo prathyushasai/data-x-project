9791e3353ca54a21c4ba851028fa08efd1ba4cd8
Gan Tu <tugan@berkeley.edu>
2017-05-02 18:51:49 -0700

    add api for clarifai

---
+++ app/server/api.py
@@ -0,0 +1,34 @@
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+from clarifai.client import ClarifaiApi
+import os
+import json
+
+app = ClarifaiApp("fQejYok0nzGJpuUxGjXKQ62U3ElPSND3F2CRdqSa", "DA9oY3z-s3X8eUUsQQzkmw1aaMBWj-s11QZb-u2C")
+clarifai_api = ClarifaiApi("fQejYok0nzGJpuUxGjXKQ62U3ElPSND3F2CRdqSa", "DA9oY3z-s3X8eUUsQQzkmw1aaMBWj-s11QZb-u2C")
+
+# get the general model
+model = app.models.get("general-v1.3")
+
+def classify_images(folder):
+    images = []
+    image_prefix = folder + "/{0}"
+    for frame in os.listdir(folder):
+        if ".jpg" in frame:
+            img = ClImage(file_obj=open(image_prefix.format(frame), 'rb'))
+            images.append(img)
+    response = model.predict(images)
+    return extract_result(response)
+
+def extract_result(response):
+    data = response["outputs"][0]["data"]["concepts"]
+    result = list()
+    # a list of tuples in the form (name, probability)
+    for datum in data:
+        result.append((datum["name"], datum["value"]))
+    return result
+
+def tag_video_file(path):
+    result = clarifai_api.tag_images(open(path))
+    print(result)
+
