016cd84fed601c1504237d2fe15eeb16e17127db
nikitavemuri <nikitavemuri@gmail.com>
2017-05-02 19:49:51 -0700

    fixed bug - generalizes to all labels

--- app/server/getCategory.py
+++ app/server/getCategory.py
@@ -12,7 +12,7 @@ def getCategory (labels, word_input, model = None):
     if model == None:
         model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)  
     for label in labels: 
-        for word in model.most_similar_cosmul(positive=["animal"], topn=100):
+        for word in model.most_similar_cosmul(positive=[label], topn=100):
             if word[0] == word_input:
                 return label
     return word_input
